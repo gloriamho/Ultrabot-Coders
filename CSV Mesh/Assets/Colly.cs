46952cfbf3ab5bdf6719cfa53a267501ba27f45d
anandmani11 <anandmani11@icloud.com>
2023-12-05 18:38:01 -0500

    merge

---
+++ CSV Mesh/Assets/Colly.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+private MeshCollider meshCollider;
+public class Colly : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshCollider = GetComponent<MeshCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshCollider.sharedMesh = mesh;
+    }
+}
